5dfe7ae34c1f85565cbf8dd1a92ab264d1e848d6
romeorafiki <49174046+romeorafiki@users.noreply.github.com>
2021-09-26 00:40:45 -0400

    Add files via upload

---
+++ search_query.py
@@ -0,0 +1,26 @@
+import tweepy
+import pandas as pd
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+number_of_tweets = 200
+tweets = []
+likes = []
+time = []
+
+for i in tweepy.Cursor(api.search, q='$GVSI',tweet_mode="extended").items(number_of_tweets):
+    tweets.append(i.full_text)
+    likes.append(i.favorite_count)
+    time.append(i.created_at)
+
+df = pd.DataFrame({'tweets':tweets,'likes':likes,'time':time})
+df.to_excel("C:\Python Projects\Data Collection\$GVSI_9-19-21.xlsx")
