9205c7515901957e4719fe0a5920e521fa6a8d2e
romeorafiki <49174046+romeorafiki@users.noreply.github.com>
2021-09-26 01:02:51 -0400

    Add files via upload

---
+++ scrape_utweets_v2.py
@@ -0,0 +1,29 @@
+#This scrapes tiwtter data and impots into .xlsx
+
+
+import tweepy
+import pandas as pd
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+number_of_tweets = 100
+tweets = []
+likes = []
+time = []
+
+for i in tweepy.Cursor(api.user_timeline, id="tupac",tweet_mode="extended").items(number_of_tweets):
+    tweets.append(i.full_text)
+    likes.append(i.favorite_count)
+    time.append(i.created_at)
+
+df = pd.DataFrame({'tweets':tweets,'likes':likes,'time':time})
+df.to_excel("C:\Python Projects\Data Collection\est_output.xlsx")
---
+++ twitter_uvariables_v2.py
@@ -0,0 +1,17 @@
+import tweepy
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+cursor = tweepy.Cursor(api.user_timeline, id='tupac',tweet_mode="extended").items(1)
+
+for i in cursor:
+    print(dir(i))
